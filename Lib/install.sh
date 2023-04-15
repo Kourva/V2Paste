7a80cfee6a859abf49caa8630fab68e12ae1e92e
Kourva <118578799+Kourva@users.noreply.github.com>
2023-04-15 07:55:12 +0330

    Create install.sh

---
+++ Lib/install.sh
@@ -0,0 +1,9 @@
+# Intalling Requirements for User
+
+echo "Installing requirements for you..."
+echo "----------------------------------"
+echo $(pip install -r requirements.txt)
+echo ""
+sleep 2
+echo "----------------------------------"
+echo "All done. You can use the app."
